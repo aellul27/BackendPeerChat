02e51b55f3b978e23f7f7bbda6527b710fa70465
Alexander Ellul <aellul27@igs.vic.edu.au>
2024-06-14 10:06:46 +1000

    Added create_root_certs
    Added module to create CA key and certificate

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import generate_user_cert
+import create_root_certs
+import os
+import time
 
-usercsr = generate_user_cert.create_user_csr("test")
-
-user_cert = generate_user_cert.create_user_cert(usercsr)
-
-with open('user_cert.pem', 'wb') as user_cert_file:
-    user_cert_file.write(user_cert)
+if os.path.isfile("./private/ca/rootCAKey.pem") and os.path.isfile("./private/ca/rootCACert.pem"):
+    print("Both RootCA key and cert are present.")
+else:
+    print("Both RootCA key and cert are NOT present.")
+    time.sleep(0.5)
+    print("\nGenerating now...\n")
+    time.sleep(0.5)
+    create_root_certs.generate_private_cakey()
